55a1d488245add8f198727c54bb28ce7e72512d0
Owen Quinlan <46948241+BuyMyMojo@users.noreply.github.com>
2021-11-19 22:09:36 +1100

    change build scripts to work with TeamCity

--- Locate ID/build-locate-id.sh
+++ Locate ID/build-locate-id.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
+
 go mod download
 
-package="$PWD/User ID Parser/IdParser.go"
+package="./LocateID.go"
 
 package_split=(${package//\// })
-package_name="IdParser"
+package_name="LocateID"
 
 platforms=("windows/amd64" "windows/386" "linux/amd64" "linux/386" "linux/arm64" "linux/arm")
 
@@ -15,11 +16,11 @@ do
     GOOS=${platform_split[0]}
     GOARCH=${platform_split[1]}
     output_name=$package_name'-'$GOOS'-'$GOARCH
-    if [ $GOOS = "windows" ]; then
+    if [ "$GOOS" = "windows" ]; then
         output_name+='.exe'
     fi
 
-    env GOOS=$GOOS GOARCH=$GOARCH GOPATH="$PWD/User ID Parser/" go build -o ./User ID Praser/build/$output_name $package
+    env GOOS="$GOOS" GOARCH="$GOARCH" go build -o ./build/$output_name $package
     if [ $? -ne 0 ]; then
         echo 'An error has occurred! Aborting the script execution...'
         exit 1
--- User ID Parser/build-id-parser.sh
+++ User ID Parser/build-id-parser.sh
@@ -2,10 +2,10 @@
 
 go mod download
 
-package="$PWD/Locate ID/LocateID.go"
+package="./IdParser.go"
 
 package_split=(${package//\// })
-package_name="LocateID"
+package_name="IdParser"
 
 platforms=("windows/amd64" "windows/386" "linux/amd64" "linux/386" "linux/arm64" "linux/arm")
 
@@ -15,11 +15,11 @@ do
     GOOS=${platform_split[0]}
     GOARCH=${platform_split[1]}
     output_name=$package_name'-'$GOOS'-'$GOARCH
-    if [ $GOOS = "windows" ]; then
+    if [ "$GOOS" = "windows" ]; then
         output_name+='.exe'
     fi
 
-    env GOOS=$GOOS GOARCH=$GOARCH GOPATH="$PWD/Locate ID/" go build -o "./Locate ID/build/$output_name" $package
+    env GOOS="$GOOS" GOARCH=$GOARCH go build -o "./build/$output_name" $package
     if [ $? -ne 0 ]; then
         echo 'An error has occurred! Aborting the script execution...'
         exit 1
